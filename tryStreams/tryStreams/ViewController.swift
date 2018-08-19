8aac9c729dfa5599add65e7364c81377c654eba5
sa <ayyappazmail@gmail.com>
2018-08-20 01:06:49 +0530

    initial commit for tryStreams

---
+++ tryStreams/tryStreams/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tryStreams
+//
+//  Created by Ayyappa Salla on 20/08/18.
+//  Copyright © 2018 thndrkiss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
